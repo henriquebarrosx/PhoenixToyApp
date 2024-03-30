2ec91d33f13a186acbf597d1e92dfe2f278508ed
Henrique Barros <henriquebarrosx@gmail.com>
2024-03-30 14:52:28 -0300

    Creates microposts scarfold

---
+++ lib/toy_app/posts/micropost.ex
@@ -0,0 +1,19 @@
+defmodule ToyApp.Posts.Micropost do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "microposts" do
+    field :content, :string
+    field :user_id, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(micropost, attrs) do
+    micropost
+    |> cast(attrs, [:content, :user_id])
+    |> validate_required([:content, :user_id])
+    |> validate_length(:content, max: 280)
+  end
+end
